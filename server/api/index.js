21458b0071047c87cb8259db52c804a4d8664065
Tamas Ridly <tamas_ridly@amp.com.au>
2016-06-10 14:36:29 +1000

    Added facebook code and fixed hostname

--- server/api/index.js
+++ server/api/index.js
@@ -9,9 +9,7 @@ export default function() {
 
 	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
-		res.json({
-			version : '1146144195'
-		});
+		res.json('1146144195');
 	});
 
 	return api;
