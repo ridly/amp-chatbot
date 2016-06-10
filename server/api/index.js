91269ae39f5e19d43ec6de3a9713b05eedbabdfa
Tamas Ridly <tamas_ridly@amp.com.au>
2016-06-10 14:39:29 +1000

    Fixed facebook code again

--- server/api/index.js
+++ server/api/index.js
@@ -9,7 +9,11 @@ export default function() {
 
 	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
-		res.json('1146144195');
+		  if (req.query['hub.verify_token'] === "my_voice_is_my_verify_me") {
+		    res.send(req.query['hub.challenge']);
+		  } else {
+		    res.send('Error, wrong validation token');    
+		  }
 	});
 
 	return api;
